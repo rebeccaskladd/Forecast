50073fe27c8e5908d81f018b4af80b160d796ba9
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-08-25 17:14:58 -0400

    fetch weather data from api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import ZipCodeForm from './components/ZipCodeForm/ZipCodeForm';
 
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
 function App() {
+    const getWeather = (zipCode) => {
+        try {
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${API_KEY}`)
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }
+        catch (error) {
+            console.log('An error has occurred:', error);
+        }
+    }
+
     return (
-        <ZipCodeForm />
+        <ZipCodeForm getWeather={getWeather} />
     )
 };
 
