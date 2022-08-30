94c7c23ebec94d596ae80ec2be21e0568a0107be
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-08-30 15:51:23 -0400

    changed api source

--- src/App.js
+++ src/App.js
@@ -6,28 +6,33 @@ import './App.css';
 
 const BING_KEY = 'Aid6JaHRZ6FLi6bt1k3NG1VyntcD82zf8_Xrc8oQ-ly842jD8zv11uETwXzoWauA';
 
+const ACCU_KEY = 'zWALoAII7yEPdoMfpW6rvYF7FtgROG4G';
+
 function App() {
     const [location, setLocation] = useState(null);
     const [currentWeather, setCurrentWeather] = useState(null);
     const [forecast, setForecast] = useState([]);
 
     const getWeather = async (zipCode) => {
-        let lat, long, currentURL, forecastURL = "";
+        let locationKey = "";
 
         // get latitude/longitude from zipcode
+        // http://dev.virtualearth.net/REST/v1/Locations?postalCode=${zipCode}&key=${BING_KEY}
         try {
-            await fetch(`http://dev.virtualearth.net/REST/v1/Locations?postalCode=${zipCode}&key=${BING_KEY}`)
+            await fetch(`http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${ACCU_KEY}&q=${zipCode}`)
                 .then(response => response.json())
                 .then(data => {
-                    const { address, point } = data.resourceSets[0].resources[0];
-                    lat = point.coordinates[0];
-                    long = point.coordinates[1];
+                    const { Key, LocalizedName, AdministrativeArea } = data[0];
+                    locationKey = Key;
+                    // const { address, point } = data.resourceSets[0].resources[0];
+                    // lat = point.coordinates[0];
+                    // long = point.coordinates[1];
 
-                    console.log(lat, long);
+                    // console.log(lat, long);
                     setLocation({
                         date: new Date(),
-                        city: address.locality,
-                        state: address.adminDistrict
+                        city: LocalizedName,
+                        state: AdministrativeArea.ID
                     });
                 })
         }
@@ -35,91 +40,126 @@ function App() {
             console.log('An error has occurred:', error);
         }
 
-        // get api urls for relevant data
         try {
-            await fetch(`https://api.weather.gov/points/${lat},${long}`)
+            await fetch(`http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${locationKey}?apikey=${ACCU_KEY}`)
                 .then(response => response.json())
                 .then(data => {
-                    const { forecast, forecastHourly } = data.properties;
-                    currentURL = forecastHourly;
-                    forecastURL = forecast;
+                    console.log(data);
+
+                    const { IconPhrase, Temperature, PrecipitationProbability, IsDaylight } = data[0];
+
+                    setCurrentWeather({
+                        weather: IconPhrase,
+                        isDaytime: IsDaylight,
+                        temperature: Temperature.Value,
+                        precipitation: PrecipitationProbability
+                    });
                 });
         }
         catch (error) {
             console.log('An error has occurred:', error);
         }
 
-        // get current weather and forecast data
-        try {
-            Promise.all([
-                fetch(currentURL),
-                fetch(forecastURL)
-            ])
-                .then(responses => responses.map(response => response.json()))
-                .then(dataArray => dataArray.forEach((data, index) => data.then(value => {
-                    // current weather data
-                    if (index === 0) {
-                        const {
-                            isDaytime,
-                            shortForecast,
-                            temperature,
-                            windSpeed,
-                            windDirection
-                        } = value.properties.periods[1];
+        // try {
+        //     await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${ACCU_KEY}`)
+        //         .then(response => response.json())
+        //         .then(data => {
+        //             data.DailyForecasts.map((element, index) => {
+        //                 return {
 
-                        setCurrentWeather({
-                            isDaytime,
-                            weather: shortForecast,
-                            temperature,
-                            windSpeed,
-                            windDirection
-                        });
-                    }
-                    else {
-                        // 6 day weather forecast data
-                        const forecastData = value.properties.periods.map((data, index) => {
-                            if (index % 2 === 0) {
-                                return { weather: data.shortForecast, temperature: data.temperature };
-                            }
-                            else {
-                                return { temperature: data.temperature };
-                            }
-                        });
+        //                 };
+        //             })
+        //         });
+        // }
+        // catch (error) {
+        //     console.log('An error has occurred:', error);
+        // }
 
-                        // organize forecast data
-                        const organizedData = [];
-                        let key = 1;
-                        for (let i = 0; i < forecastData.length; i += 2) {
-                            let date = "";
+        // get api urls for relevant data
+        // try {
+        //     await fetch(`https://api.weather.gov/points/${lat},${long}`)
+        //         .then(response => response.json())
+        //         .then(data => {
+        //             const { forecast, forecastHourly } = data.properties;
+        //             currentURL = forecastHourly;
+        //             forecastURL = forecast;
+        //         });
+        // }
+        // catch (error) {
+        //     console.log('An error has occurred:', error);
+        // }
 
-                            if (i === 0) {
-                                date = "Today";
-                            }
-                            else {
-                                date = new Date(Date.now() + (86400000 * (i / 2)));
-                            }
+        // // get current weather and forecast data
+        // try {
+        //     Promise.all([
+        //         fetch(currentURL),
+        //         fetch(forecastURL)
+        //     ])
+        //         .then(responses => responses.map(response => response.json()))
+        //         .then(dataArray => dataArray.forEach((data, index) => data.then(value => {
+        //             // current weather data
+        //             if (index === 0) {
+        //                 const {
+        //                     isDaytime,
+        //                     shortForecast,
+        //                     temperature,
+        //                     windSpeed,
+        //                     windDirection
+        //                 } = value.properties.periods[1];
 
-                            key++;
+        //                 setCurrentWeather({
+        //                     isDaytime,
+        //                     weather: shortForecast,
+        //                     temperature,
+        //                     windSpeed,
+        //                     windDirection
+        //                 });
+        //             }
+        //             else {
+        //                 // 6 day weather forecast data
+        //                 const forecastData = value.properties.periods.map((data, index) => {
+        //                     if (index % 2 === 0) {
+        //                         return { weather: data.shortForecast, temperature: data.temperature };
+        //                     }
+        //                     else {
+        //                         return { temperature: data.temperature };
+        //                     }
+        //                 });
 
-                            organizedData.push(
-                                {
-                                    key,
-                                    date,
-                                    weather: forecastData[i].weather,
-                                    low_temp: forecastData[i + 1].temperature,
-                                    high_temp: forecastData[i].temperature
-                                }
-                            );
-                        }
+        //                 // organize forecast data
+        //                 const organizedData = [];
+        //                 let key = 1;
+        //                 for (let i = 0; i < forecastData.length; i += 2) {
+        //                     let date = "";
 
-                        setForecast(organizedData);
-                    }
-                }
-                )));
-        }
-        catch (error) {
-            console.log('An error has occurred:', error);
-        }
+        //                     if (i === 0) {
+        //                         date = "Today";
+        //                     }
+        //                     else {
+        //                         date = new Date(Date.now() + (86400000 * (i / 2)));
+        //                     }
+
+        //                     key++;
+
+        //                     organizedData.push(
+        //                         {
+        //                             key,
+        //                             date,
+        //                             weather: forecastData[i].weather,
+        //                             low_temp: forecastData[i + 1].temperature,
+        //                             high_temp: forecastData[i].temperature
+        //                         }
+        //                     );
+        //                 }
+
+        //                 setForecast(organizedData);
+        //             }
+        //         }
+        //         )));
+        // }
+        // catch (error) {
+        //     console.log('An error has occurred:', error);
+        // }
     }
 
     return (
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -7,10 +7,10 @@ const Weather = ({ location, currentWeather, forecast }) => {
     //console.log(currentWeather);
     //console.log(forecast);
 
-    const { city, state, date: todaysDate } = location;
-    const { temperature, weather, windSpeed, windDirection, isDayTime } = currentWeather;
+    const { city, state, date } = location;
+    const { temperature, weather, isDaytime, precipitation } = currentWeather;
 
-    const { high_temp } = forecast[0];
+    // const { high_temp } = forecast[0];
 
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -19,18 +19,18 @@ const Weather = ({ location, currentWeather, forecast }) => {
         <div className="weather-container">
             <div className="location-info-container">
                 <div className="location-info">
-                    <p>{daysOfWeek[todaysDate.getDay()]}, {months[todaysDate.getMonth()]} {todaysDate.getDate()}, {todaysDate.getFullYear()}</p>
+                    <p>{daysOfWeek[date.getDay()]}, {months[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</p>
                     <p>{`${city}, ${state}`}</p>
                 </div>
                 <div className="weather-details">
-                    <p>Wind: {windSpeed} {windDirection}</p>
+                    <p>Precipitation: {Math.floor(precipitation / 10) * 10}% chance</p>
                 </div>
             </div>
             <div className="weather-block">
                 <p className="temp">{temperature}&#176;F</p>
                 <div className="weather-desc">
                     <p>{weather}</p>
-                    <p>The high today will be {high_temp}&#176;F.</p>
+                    <p>The high today will be &#176;F.</p>
                 </div>
             </div>
 
